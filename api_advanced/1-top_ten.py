6efc0985041738b5fdb63a9c04d40f20b5dc66f1
Wilson Nshizirungu <wilsonshizirungu@gmail.com>
2026-02-23 09:57:00 +0200

    chore: Update User-Agent header to a descriptive format for Reddit API requests.

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -9,7 +9,7 @@ import requests
 def top_ten(subreddit):
     """ prints the titles of the first 10 hot posts listed in a subreddit """
     url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0'}
+    headers = {'User-Agent': 'linux:alu.api.advanced:v1.0 (by /u/alu_student)'}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code != 200:
         print(None)
